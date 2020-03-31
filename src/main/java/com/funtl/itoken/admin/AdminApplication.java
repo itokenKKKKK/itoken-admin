4fdd59471ae4734d6a9d193b9782e27ad1707f04
skl <1026809406@qq.com>
2020-03-31 16:35:59 +0800

    初始化

---
+++ src/main/java/com/funtl/itoken/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package com.funtl.itoken.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author GuardianYruler
+ * @create 2020/3/31 16:26
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
